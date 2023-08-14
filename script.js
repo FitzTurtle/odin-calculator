ec77d4e93519cd0d2ee1d649e08d45b0c78b793b
fitzTurtle <137014500+FitzTurtle@users.noreply.github.com>
2023-08-14 19:55:14 -0400

    implemented initial operate function

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
+let firstNum;
+let secondNum;
+let operator;
 
 
+// Basic Functions
 function add(num1, num2) {
     return num1+num2;
 }
@@ -14,4 +18,23 @@ function multiply(num1, num2) {
 
 function divide(num1, num2) {
     return num1/num2;
+}
+
+//Operations
+function operate(operation, num1, num2) {
+
+    switch (operation) {
+        case "+":
+            add(num1,num2);
+            break;
+        case "-":
+            subtract(num1,num2);
+            break;
+        case "*":
+            multiply(num1,num2);
+            break;
+        case "/":
+            divide(num1,num2);
+            break;
+    }
 }
