1af9549dfffdd77965c63e11786e37ce4f755a67
fitzTurtle <137014500+FitzTurtle@users.noreply.github.com>
2023-08-19 11:15:40 -0400

    Changing strategy for creating equations
    Removing addToDisplay function as the main function for creating my
    my equation string. Will focus on building individual strings for
    first and second number, allowing for negative signs first

--- script.js
+++ script.js
@@ -1,47 +1,77 @@
-let firstNum;
-let secondNum;
+let firstNum="0";
+let secondNum="0";
 let operator;
 let currentEquation="";
+let firstDone = false;
 const operationRegex = new RegExp(/[\/\+\-\*]/);
 
 const buttons = document.querySelectorAll(".button");
 // console.log(numButtons);
 // const opButtons = document.querySelectorAll(".button.operation");
 
-buttons.forEach( (button) => button.addEventListener('click', addToDisplay));
+buttons.forEach( (button) => button.addEventListener('click', buildEquation));
 
 
-
-function addToDisplay(e){
+function buildEquation(e) {
 
     const buttonClasses = e.target.classList;
+    const isNumber = buttonClasses.contains("number");
+    const isOperator = buttonClasses.contains("operator")
+    const isEqual = buttonClasses.contain("equal");
+
     const input = e.target.id;
+
+    //Build first number
+    if(!firstDone) {
+
+        if(e.target.id==="-" &&  firstnum === "0") {
+            firstNum = "-";
+        }
+
+        if(isNumber) {
+            firstNum += input;
+        } else if (isOperator) {
+            operator = e.target.id;
+            firstDone = true;
+        } else if (isEqual) {
+            
+        }
+    }
+}
+
+
+
+// function addToDisplay(e){
+
+//     const buttonClasses = e.target.classList;
+//     const input = e.target.id;
    
 
 
-    //number buttons
-    if (buttonClasses.contains('number') && currentEquation.length <=10) {
-        currentEquation += input;
-        updateDisplay(currentEquation);
-    } 
-    //operator buttons
-    else if (buttonClasses.contains('operation')) {
-        if (operationRegex.test(currentEquation)) {
+//     //number buttons
+//     if (buttonClasses.contains('number') && currentEquation.length <=10) {
+//         currentEquation += input;
+//         updateDisplay(currentEquation);
+//     } 
+//     //operator buttons
+//     else if (buttonClasses.contains('operation')) {
+//         if (operationRegex.test(currentEquation)) {
 
-        } else {
-            currentEquation += input;
-            updateDisplay(currentEquation);
-        }
-    }
-    //equal button
-    else if (buttonClasses.contains('equal')) {
-        parseCalculation(currentEquation);
-    }
-    console.log(currentEquation);
-    console.log(operationRegex.test(currentEquation));
-}
+//         } else {
+//             currentEquation += input;
+//             updateDisplay(currentEquation);
+//         }
+//     }
+//     //equal button
+//     else if (buttonClasses.contains('equal')) {
+//         parseCalculation(currentEquation);
+//     }
+//     console.log(currentEquation);
+//     console.log(operationRegex.test(currentEquation));
+// }
 
 function parseCalculation(equation) {
+    // Need to rework this to allow a split even with negative numbers
     let [num1, num2] = equation.split(operationRegex);
     let operand = equation.charAt(equation.search(operationRegex));
     console.log(num1);
